63f9c8b92a1302cd48c8c0c8648300b8a2524e31
Anthony Giuliano <agiuliano@gmail.com>
2016-06-19 13:31:55 -0400

    Adds index page.

--- cms.rb
+++ cms.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require "tilt/erubis"
+
+helpers do
+  def root
+    File::expand_path('..', __FILE__)
+  end
+end
 
 get '/' do
-  'Getting Started'
+  @files = Dir.glob(root + '/data/*').map { |file| File::basename(file) }
+  erb :index, layout: :layout
 end

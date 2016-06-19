9a28dca90f470da9aa806fa39a6579bcb8123504
Anthony Giuliano <agiuliano@gmail.com>
2016-06-19 14:05:02 -0400

    Adds ability to view text files.

--- cms.rb
+++ cms.rb
@@ -12,3 +12,9 @@ get '/' do
   @files = Dir.glob(root + '/data/*').map { |file| File::basename(file) }
   erb :index, layout: :layout
 end
+
+get '/:filename' do
+  file_path = root + '/data/' + params[:filename]
+  headers["Content-Type"] = "text/plain"
+  File.read(file_path)
+end

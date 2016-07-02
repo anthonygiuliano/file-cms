11b1f523d70c177c73fe20344c628f0d08686dcf
Anthony Giuliano <agiuliano@gmail.com>
2016-07-02 13:53:08 -0400

    Adds css

--- cms.rb
+++ cms.rb
@@ -1,16 +1,18 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'tilt/erubis'
-require 'redcarpet'
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+require "redcarpet"
 
 configure do
   enable :sessions
   set :session_secret, 'super secret'
 end
 
-helpers do
-  def root
-    File::expand_path('..', __FILE__)
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
   end
 end
 
@@ -22,41 +24,47 @@ end
 def load_file_content(path)
   content = File.read(path)
   case File.extname(path)
-  when '.txt'
+  when ".txt"
     headers["Content-Type"] = "text/plain"
     content
-  when '.md'
-    render_markdown(content)
+  when ".md"
+    erb render_markdown(content)
   end
 end
 
-get '/' do
-  @files = Dir.glob(root + '/data/*').map { |file| File::basename(file) }
-  erb :index, layout: :layout
+get "/" do
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
+    File.basename(path)
+  end
+  erb :index
 end
 
-get '/:filename' do
-  file_path = root + "/data/" + params[:filename]
+get "/:filename" do
+  file_path = File.join(data_path, params[:filename])
 
   if File.exist?(file_path)
     load_file_content(file_path)
   else
-    session[:message] = "#{params[:filename]} does not exist."
+    session[:message] = "#{params[:filename]} does not exist." unless params[:filename] =~ /favicon/
     redirect "/"
   end
 end
 
-get '/:filename/edit' do
-  file_path = root + "/data/" + params[:filename]
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
   @filename = params[:filename]
   @content = File.read(file_path)
 
-  erb :edit, layout: :layout
+  erb :edit
 end
 
-post '/:filename' do
-  file_path = root + "/data/" + params[:filename]
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
   File.write(file_path, params[:content])
+
   session[:message] = "#{params[:filename]} has been updated."
-    redirect '/'
+  redirect "/"
 end
